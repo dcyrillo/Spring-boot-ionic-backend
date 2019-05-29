8b3a1e25c7b68267661e5a5e0c493b20b4f10db3
dcyrillo <d.cyrillo@hotmail.com>
2019-05-29 05:46:41 -0300

    primeira versão

---
+++ src/main/java/com/dcyrillo/curmc/Curmc1Application.java
@@ -0,0 +1,13 @@
+package com.dcyrillo.curmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curmc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curmc1Application.class, args);
+	}
+
+}
