46a8713bd98b894a7c9620415e40534c7a1d23a9
dcyrillo <d.cyrillo@hotmail.com>
2019-05-31 06:39:56 -0300

    item, pedido ,pk

--- src/main/java/com/dcyrillo/curmc/Curmc1Application.java
+++ src/main/java/com/dcyrillo/curmc/Curmc1Application.java
@@ -13,6 +13,7 @@ import com.dcyrillo.curmc.domain.Cidade;
 import com.dcyrillo.curmc.domain.Cliente;
 import com.dcyrillo.curmc.domain.Endereco;
 import com.dcyrillo.curmc.domain.Estado;
+import com.dcyrillo.curmc.domain.ItemPedido;
 import com.dcyrillo.curmc.domain.Pagamento;
 import com.dcyrillo.curmc.domain.PagamentoComCartao;
 import com.dcyrillo.curmc.domain.Pedido;
@@ -24,6 +25,7 @@ import com.dcyrillo.curmc.repositories.CidadeRepository;
 import com.dcyrillo.curmc.repositories.ClienteRepository;
 import com.dcyrillo.curmc.repositories.EnderecoRepository;
 import com.dcyrillo.curmc.repositories.EstadoRepository;
+import com.dcyrillo.curmc.repositories.ItemPedidoRepository;
 import com.dcyrillo.curmc.repositories.PagamentoRepository;
 import com.dcyrillo.curmc.repositories.PedidoRepository;
 import com.dcyrillo.curmc.repositories.ProdutoRepository;
@@ -47,6 +49,8 @@ public class Curmc1Application implements CommandLineRunner {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -109,6 +113,19 @@ public class Curmc1Application implements CommandLineRunner {
 	    pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 	    pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
+	    
+	    ItemPedido ip1=new ItemPedido(ped1,p1,0.00,1,2000.00);
+	    ItemPedido ip2=new ItemPedido(ped1,p3,0.00,2,80.00);
+	    ItemPedido ip3=new ItemPedido(ped2,p2,100.00,1,800.00);
+	    
+	    ped1.getItens().addAll(Arrays.asList(ip1,ip2));	
+	    ped2.getItens().addAll(Arrays.asList(ip3));	
+	    
+	    p1.getItens().addAll(Arrays.asList(ip1));
+	    p2.getItens().addAll(Arrays.asList(ip3));
+	    p3.getItens().addAll(Arrays.asList(ip2));
+	    	
+	    itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 	
 	}
 
--- src/main/java/com/dcyrillo/curmc/repositories/ItemPedidoRepository.java
+++ src/main/java/com/dcyrillo/curmc/repositories/ItemPedidoRepository.java
@@ -1,10 +1,11 @@
 package com.dcyrillo.curmc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
-import com.dcyrillo.curmc.domain.Estado;
+
+import com.dcyrillo.curmc.domain.ItemPedido;
+
 @Repository
-public interface EstadoRepository extends JpaRepository<Estado,Integer> {
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido,Integer> {
 
 }
