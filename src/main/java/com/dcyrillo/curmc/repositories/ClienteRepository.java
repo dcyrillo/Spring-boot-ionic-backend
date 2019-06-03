d6372fdbe7326cc5386e1700f93c28347192b320
dcyrillo <d.cyrillo@hotmail.com>
2019-06-03 12:12:42 -0300

    validação customizada email

--- src/main/java/com/dcyrillo/curmc/repositories/ClienteRepository.java
+++ src/main/java/com/dcyrillo/curmc/repositories/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.dcyrillo.curmc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dcyrillo.curmc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
+	@Transactional
+	Cliente findByEmail(String email);
 
 }
